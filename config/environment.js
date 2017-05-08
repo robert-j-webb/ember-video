6f39108d31a2c602988bf4c251011d87c7f70813
Rob <robert@lionce.com>
2017-05-07 20:05:09 -0400

    added ember paper

--- config/environment.js
+++ config/environment.js
@@ -29,8 +29,9 @@ module.exports = function (environment) {
   };
   ENV.contentSecurityPolicy = {
     'default-src': "'none'",
-    'script-src': "'self'",
-    'font-src': "'self' http://fonts.gstatic.com",
+    'script-src': "'self' 'unsafe-inline'",
+    'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
+    'font-src': "'self' fonts.gstatic.com",
     'connect-src': "'self'",
     'img-src': "'self' data:",
     'media-src': "'self'"

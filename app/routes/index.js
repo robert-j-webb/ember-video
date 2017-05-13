7782f6ae0b2ec1dccc9184b94175dc269e9b7b24
haburka@github.com <robert@lionce.com>
2017-05-13 02:05:35 -0400

    created a somewhat paletable mobile view

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('thread')
+    return this.get('store').findAll('thread', {include: 'replies'})
       .forEach((thread) => thread.video = { src: thread.video, type: 'video/mp4' });
   },
 });

0fbfd8f14af648f90d239908be16c0dd549aa70e
haburka@github.com <robert@lionce.com>
2017-06-28 22:25:12 -0400

    fixed prefix

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   var ENV = {
-    modulePrefix: 'ember-quickstart',
+    modulePrefix: 'ember-video',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',

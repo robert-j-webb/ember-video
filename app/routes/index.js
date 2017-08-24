b0335cbe0b5c649efb1898c67e03697f260fcbff
haburka@github.com <robert@lionce.com>
2017-08-24 17:34:09 -0400

    made it so mosaic route was the only one visible

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    this.transitionTo('/mosaic/mobile');
+  },
   model() {
     return this.get('store').findAll('thread', {include: 'replies'})
       .forEach((thread) => thread.video = { src: thread.video, type: 'video/mp4' });
